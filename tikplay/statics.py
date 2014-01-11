bbf2694ffc2be27cda5d506447d1725f64f90ad8
Jami Lindh <stealthyloner@hukassa.com>
2014-01-11 16:42:06 +0200

    Implement Server and Handler classes

--- tikplay/statics.py
+++ tikplay/statics.py
@@ -1,7 +1,11 @@
 USAGE = """ This stinking pile of duct-tape offers a RESTful API.
 Usage is the following:
-    GET /find/<"hash"|"name">/<hash|name>
+    GET /find/song_hash/<song_hash>
+    GET /find/filename/<filename>
+    GET /find/title/<title>
+    GET /find/artist/<artist>
+    GET /find/length/<length>
     GET /now_playing
-    GET /play/<"hash"|"name">/<hash|name>
+    GET /play/<"song_hash"|"filename">/<song_hash|filename>
     POST /file
 """

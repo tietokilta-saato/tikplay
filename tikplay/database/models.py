adb458132b4e633052c9e46e1dc4e67306f9fc6d
Jami Lindh <>
2014-09-08 10:49:02 +0300

    Add __repr__ and __str__ for DB model

--- tikplay/database/models.py
+++ tikplay/database/models.py
@@ -12,3 +12,13 @@ class Song(Base):
     length = sa.Column(sa.Integer, nullable=True)
     last_played = sa.Column(sa.DateTime, nullable=True)
     date_added = sa.Column(sa.DateTime, nullable=True)
+
+    def __repr__(self):
+        return "<Song(song_hash={!r}, filename={!r}, play_count={!r}, artist={!r}, title={!r}, length={!r}, last_played={!r}, date_added={!r})>".format(
+               self.song_hash, self.filename, self.play_count, self.artist,
+               self.title, self.length, self.last_played, self.date_added)
+
+    def __str__(self):
+        return "<Song(song_hash={!s}, filename={!s}, play_count={!s}, artist={!s}, title={!s}, length={!s}, last_played={!s}, date_added={!s})>".format(
+               self.song_hash, self.filename, self.play_count, self.artist,
+               self.title, self.length, self.last_played, self.date_added)

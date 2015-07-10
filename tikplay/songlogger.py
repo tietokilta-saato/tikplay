9306aa5f2d3b4e05f278e482797049aa4c69b436
A.M. Helin <shadikka@gmail.com>
2015-07-10 17:10:54 +0300

    Fixed filename encoding problems

--- tikplay/songlogger.py
+++ tikplay/songlogger.py
@@ -7,11 +7,11 @@ import os.path
 
 class SongLogger:
     def __init__(self, fn):
-        self.fp = open(os.path.expanduser(fn), "a")
+        self.fp = open(os.path.expanduser(fn), "ab")
 
     def write(self, user, song):
         line = "{0:%Y}-{0:%m}-{0:%d}T{0:%H}:{0:%M}:{0:%S}Z {1} {2}\n".format(
             datetime.datetime.utcnow(), user, song
         )
-        self.fp.write(line)
+        self.fp.write(line.encode('utf8', error='backslashreplace'))
         self.fp.flush()
